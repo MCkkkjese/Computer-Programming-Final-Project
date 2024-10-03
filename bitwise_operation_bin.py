9db76277c8130973e82d79270376a977bc2137c4
Zhe Wei Chang <126851525+Liyue-Wei@users.noreply.github.com>
2024-10-04 02:44:19 +0800

    bitwise operation py偵錯完成

--- bitwise_operation_bin.py
+++ bitwise_operation_bin.py
@@ -1,12 +1,16 @@
 a, b = 65, 97
 print(chr(a), chr(b))
 print("ASCII code : a = \"{}\", b = \"{}\"".format(a, b))
-print("bin a = {}\nbin b = {}\n".format(bin(a).strip("0b"), bin(b).strip("0b")))
+print("bin a = {}\nbin b = {}\n".format(bin(a).lstrip("0b"), bin(b).lstrip("0b")))
 
 print("Dec a&b = {}".format(a&b))
 print("Dec a|b = {}".format(a|b))
 print("Dec a^b = {}".format(a^b))
 
-print("Bin a&b = {}".format(int(bin(a).strip("0b"))&int(bin(b).strip("0b"))))
-print("Bin a|b = {}".format(int(bin(a).strip("0b"))|int(bin(b).strip("0b"))))
-print("Bin a^b = {}".format(int(bin(a).strip("0b"))^int(bin(b).strip("0b"))))
+a_and_b = bin(a&b).lstrip("0b")
+a_or_b = bin(a|b).lstrip("0b")
+a_xor_b = bin(a^b).lstrip("0b")
+
+print("Bin a&b = {}".format(a_and_b))
+print("Bin a|b = {}".format(a_or_b))
+print("Bin a^b = {}".format(a_xor_b))
