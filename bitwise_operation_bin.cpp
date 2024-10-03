e7d8de6e374c21a38918a7b4205f3486d156214c
Zhe Wei Chang <126851525+Liyue-Wei@users.noreply.github.com>
2024-10-03 22:53:38 +0800

    XOR整理

---
+++ bitwise_operation_bin.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+#include <bitset>
+using namespace std;
+int main() {
+    char a=65, b=97;
+    printf("a = \"%c\", b = \"%c\"\n", a, b);
+    printf("ascii code : a = \"%d\", b = \"%d\"\n", static_cast<int>(a), static_cast<int>(b));
+
+    bitset<8> bin_a(a); 
+    bitset<8> bin_b(b);
+    cout << bin_a << '\n' << bin_b << "\n\n";  // printf()不支援以二進位輸出
+
+    cout << "Dec a&b = " << static_cast<int>(a&b) << '\n';
+    cout << "Dec a|b = " << static_cast<int>(a|b) << '\n';
+    cout << "Dec a^b = " << static_cast<int>(a^b) << "\n\n";
+
+    bitset<8> bin_a_and_b(a&b);
+    bitset<8> bin_a_or_b(a|b);
+    bitset<8> bin_a_xor_b(a^b);
+
+    cout << "Bin a&b = " << bin_a_and_b << '\n';
+    cout << "Bin a|b = " << bin_a_or_b << '\n';
+    cout << "Bin a^b = " << bin_a_xor_b << '\n';
+
+    system("Pause");
+    return 0;
+}
