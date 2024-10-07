3e890f32c419816ada3df2e17c0b67d3393f3bb9
Zhe Wei Chang <126851525+Liyue-Wei@users.noreply.github.com>
2024-10-07 16:52:11 +0800

    Update CH3_3_2.cpp

--- 程式設計實驗/CH3_3_2.cpp
+++ 程式設計實驗/CH3_3_2.cpp
@@ -9,10 +9,10 @@ int main(){
     BMI = weight/pow(height, 2);
 
     if(BMI<18.5) {printf("過輕\n");}
-    else if(18.5>=BMI>24) {printf("正常\n");}
-    else if(24>=BMI>37) {printf("過重\n");}
-    else if(27>=BMI>30) {printf("輕度肥胖\n");}
-    else if(30>=BMI>35) {printf("中度肥胖\n");}
+    else if(BMI<24) {printf("正常\n");}
+    else if(BMI<27) {printf("過重\n");}
+    else if(BMI<30) {printf("輕度肥胖\n");}
+    else if(BMI<35) {printf("中度肥胖\n");}
     else {printf("重度肥胖\n");}
 
     system("PAUSE");
