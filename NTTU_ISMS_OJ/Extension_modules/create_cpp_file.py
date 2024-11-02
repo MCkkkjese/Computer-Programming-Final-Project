c3ac1d3e63d09adf849576ae55d868f36749834e
Zhe Wei Chang <126851525+Liyue-Wei@users.noreply.github.com>
2024-11-02 22:31:06 +0800

    Update create_cpp_file.py

--- NTTU_ISMS_OJ/Extension_modules/create_cpp_file.py
+++ NTTU_ISMS_OJ/Extension_modules/create_cpp_file.py
@@ -1,6 +1,7 @@
 #coding=UTF-8
 import time
 import os
+import re
 
 def write_source_code(index, user):
     try:
@@ -9,6 +10,10 @@ def write_source_code(index, user):
     except:
         import file_directory
 
+    dir_path = str(os.path.dirname(__file__))
+    print(dir_path)
+    dir_path = re.sub("Extension_modules", '', dir_path)
+    print(dir_path)
     time_now = str(time.strftime("_%m_%d_%Y_%H_%M_%S_", time.localtime()))
     filename = str("source_code_{}_{}.cpp".format(time_now, user))
     filename_2 = file_directory.path_function("Source_code/{}".format(filename))
@@ -17,6 +22,6 @@ def write_source_code(index, user):
     outFile.flush()
     outFile.close()
 
-    file_path = ("g++ " + filename + " -o " + filename.rstrip(".cpp"))
+    file_path = ("cd " + dir_path + "\Source_code && " + "g++ " + filename + " -o " + filename.rstrip(".cpp"))
     print(file_path)
     os.system(file_path)
