cc1b354871a080d81e690c0f2afbc056476fc26e
Zhe Wei Chang <126851525+Liyue-Wei@users.noreply.github.com>
2024-11-02 22:11:12 +0800

    1102進度更新

--- NTTU_ISMS_OJ/Extension_modules/create_cpp_file.py
+++ NTTU_ISMS_OJ/Extension_modules/create_cpp_file.py
@@ -12,19 +12,10 @@ def write_source_code(index, user):
     time_now = str(time.strftime("_%m_%d_%Y_%H_%M_%S_", time.localtime()))
     filename = str("source_code_{}_{}.cpp".format(time_now, user))
     filename_2 = file_directory.path_function("Source_code/{}".format(filename))
-    
-    try:
-        outFile = open(filename_2, 'w')
-        outFile.write(index)
-        outFile.flush()
-        outFile.close()
-
-    except:
-        os.mkdir("/Source_code")
-        outFile = open(filename_2, 'w')
-        outFile.write(index)
-        outFile.flush()
-        outFile.close()
+    outFile = open(filename_2, 'w')
+    outFile.write(index)
+    outFile.flush()
+    outFile.close()
 
     file_path = ("g++ " + filename + " -o " + filename.rstrip(".cpp"))
     print(file_path)
---
+++ NTTU_ISMS_OJ/Source_code/Source_code.md
@@ -0,0 +1,2 @@
+# source code folder
+## filename formate = source_code__MM_DD_YYYY__user.cpp
