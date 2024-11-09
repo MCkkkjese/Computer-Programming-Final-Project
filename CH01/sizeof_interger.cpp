0810fd1a4efdcf79ed98ff9237882a80db254997
Zhe Wei Chang <126851525+Liyue-Wei@users.noreply.github.com>
2024-11-10 04:32:06 +0800

    Update sizeof_interger.cpp

--- CH01/sizeof_interger.cpp
+++ CH01/sizeof_interger.cpp
@@ -2,6 +2,7 @@
 #include <limits>
 using namespace std;
 int main(){
+    short int a;
     int i;
     long n;
     long long t;
@@ -9,6 +10,7 @@ int main(){
     unsigned long y;
     unsigned long long z; 
 
+    cout << "sizeof(short int) = " << sizeof(a) << " bytes" << "\n";
     cout << "sizeof(int) = " << sizeof(i) << " bytes" << "\n";
     cout << "sizeof(long) = " << sizeof(n) << " bytes" << "\n";
     cout << "sizeof(long long) = " << sizeof(t) << " bytes" << "\n";
@@ -16,6 +18,8 @@ int main(){
     cout << "sizeof(unsigned long) = " << sizeof(y) << " bytes" << "\n";
     cout << "sizeof(unsigned long long) = " << sizeof(z) << " bytes" << "\n\n";
 
+    cout << "max short int = " << numeric_limits<short int>::max() << "\n";
+    cout << "min short int = " << numeric_limits<short int>::min() << "\n\n";
     cout << "max int = " << numeric_limits<int>::max() << "\n";
     cout << "min int = " << numeric_limits<int>::min() << "\n\n";
     cout << "max long int = " << numeric_limits<long>::max() << "\n";
@@ -23,6 +27,8 @@ int main(){
     cout << "max long long int = " << numeric_limits<long long>::max() << "\n";
     cout << "min long long int = " << numeric_limits<long long>::min() << "\n\n";
 
+    cout << "max unsigned short int = " << numeric_limits<unsigned short int>::max() << "\n";
+    cout << "min unsigned short int = " << numeric_limits<unsigned short int>::min() << "\n\n";
     cout << "max unsigned int = " << numeric_limits<unsigned int>::max() << "\n";
     cout << "min unsigned int = " << numeric_limits<unsigned int>::min() << "\n\n";
     cout << "max unsigned long int = " << numeric_limits<unsigned long>::max() << "\n";
@@ -35,12 +41,16 @@ int main(){
 }
 
 /*
+sizeof(short int) = 2 bytes
 sizeof(int) = 4 bytes
 sizeof(long) = 4 bytes
 sizeof(long long) = 8 bytes
 sizeof(unsigned int) = 4 bytes
 sizeof(unsigned long) = 4 bytes
 sizeof(unsigned long long) = 8 bytes
+
+max short int = 32767
+min short int = -32768
 
 max int = 2147483647
 min int = -2147483648
@@ -51,6 +61,9 @@ min long int = -2147483648
 max long long int = 9223372036854775807
 min long long int = -9223372036854775808
 
+max unsigned short int = 65535
+min unsigned short int = 0
+
 max unsigned int = 4294967295
 min unsigned int = 0
 
@@ -60,5 +73,5 @@ min unsignedlong int = 0
 max unsigned long long int = 18446744073709551615
 min unsigned long long int = 0
 
-請按任意鍵繼續 . . .
+請按任意鍵繼續 . . . 
 */
