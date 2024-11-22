262d51a429d60b8857ab72bf8b7520a1826b2c02
Zhe Wei Chang <126851525+Liyue-Wei@users.noreply.github.com>
2024-11-23 01:42:39 +0800

    1123

---
+++ CH06/Uppercase.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <cstring>
+using namespace std;
+void upper(string str) {
+    cout << str.size();
+}
+
+int main() {
+    string str;
+    cin >> str;
+    upper(str);
+}
