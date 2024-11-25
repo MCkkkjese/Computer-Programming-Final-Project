7790bfa25e61078648286070f9c95d906b0d3d0d
Zhe Wei Chang <126851525+Liyue-Wei@users.noreply.github.com>
2024-11-25 19:17:08 +0800

    Create Lottery.cpp

---
+++ CH06/Lottery.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <cstdlib>
+#include <fstream>
+#include <conio.h>
+#include <ctime>
+#include <cstring>
+#include <vector>
+#include <map>
+using namespace std;
+int main() {
+
+}
