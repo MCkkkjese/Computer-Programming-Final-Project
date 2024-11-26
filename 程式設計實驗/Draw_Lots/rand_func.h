e0e49c6c7d98fae436583e604205fb6335887bf1
Zhe Wei Chang <126851525+Liyue-Wei@users.noreply.github.com>
2024-11-26 22:21:03 +0800

    Update rand_func.h

--- 程式設計實驗/Draw_Lots/rand_func.h
+++ 程式設計實驗/Draw_Lots/rand_func.h
@@ -16,7 +16,7 @@ int rand_func(int i, int n) {
         } else if (operation == 13) {
             _draw();
         } else {
-            operation == 27 ? printf("Program terminated.\n") : printf("Invalid key pressed, press 'y' key to draw a number, or press 'Enter' key to draw a number, or press 'Esc' to exit.\n");
+            operation == 27 ? printf("Program terminated.\n") : printf("Invalid key pressed, press 'y' key to draw a number, or press 'Esc' to exit.\n");
         }
     } while (operation != 27);
 
