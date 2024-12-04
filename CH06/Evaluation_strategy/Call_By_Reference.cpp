f66dd6a9611c4d07ba45632dfb8ad49a280c1355
Zhe Wei Chang <126851525+Liyue-Wei@users.noreply.github.com>
2024-12-04 11:33:13 +0800

    evaluation strategy

--- CH06/Evaluation_strategy/Call_By_Reference.cpp
+++ CH06/Evaluation_strategy/Call_By_Reference.cpp
@@ -1,16 +1,16 @@
 #include <stdio.h>
-void func(int *i, int *n) {
-    printf("void func : i = %d, n = %d\n", *i, *n);
-    *i = 16;
-    *n = 32;
+void func(int &i, int &n) {
+    printf("void func : i = %d, n = %d\n", i, n);
+    i = 16;
+    n = 32;
     printf("void func : set i = 16, set n = 32\n");
-    printf("void func : i = %d, n = %d\n", *i, *n);
+    printf("void func : i = %d, n = %d\n", i, n);
 }
 
 int main() {
-    printf("Call By Address\n");
+    printf("Call By Reference\n");
     int i = 128, n = 256;
     printf("int main : i = %d, n = %d\n", i, n);
-    func(&i, &n);
+    func(i, n);
     printf("int main : i = %d, n = %d\n", i, n);
 }
--- CH06/Evaluation_strategy/Call_By_Value.c
+++ CH06/Evaluation_strategy/Call_By_Value.c
@@ -1,8 +1,16 @@
 #include <stdio.h>
-void func() {
-
+void func(int i, int n) {
+    printf("void func : i = %d, n = %d\n", i, n);
+    i = 16;
+    n = 32;
+    printf("void func : set i = 16, set n = 32\n");
+    printf("void func : i = %d, n = %d\n", i, n);
 }
 
 int main() {
-    
+    printf("Call By Value\n");
+    int i = 128, n = 256;
+    printf("int main : i = %d, n = %d\n", i, n);
+    func(i, n);
+    printf("int main : i = %d, n = %d\n", i, n);
 }
