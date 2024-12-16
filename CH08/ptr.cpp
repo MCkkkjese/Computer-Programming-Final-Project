a1007d3c7874c63add62dbeb44ec24992feb113b
Zhe Wei Chang <126851525+Liyue-Wei@users.noreply.github.com>
2024-12-16 23:47:22 +0800

    Update ptr.cpp

--- CH08/ptr.cpp
+++ CH08/ptr.cpp
@@ -1,5 +1,18 @@
+/*
+Pointers are variables that store the memory address of another variable.
+Pointers are used to access the memory location of a variable.
+
+The address of a variable can be obtained by preceding the name of the variable with an ampersand sign (&), known as the address-of operator.
+The value of a variable can be obtained by preceding the pointer with an asterisk sign (*), known as the dereference operator.
+
+data_type *pointer_name;    // pointer declaration
+data_type *pointer_name = &variable_name;    // pointer initialization
+    
+declare a pointer to an data_type variable  -->  data_type *pointer_name = &variable_name;    // e.g. int *ptr = &num;  
+*/
+
 #include <iostream>
 using namespace std;
 int main() {
-    
+
 }
