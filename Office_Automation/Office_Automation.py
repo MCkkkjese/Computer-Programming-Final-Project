7cf88bee7ead0de1522a11ccf1cfcf84d69c25d8
Zhe Wei Chang <126851525+Liyue-Wei@users.noreply.github.com>
2024-12-20 18:55:47 +0800

    Update Office_Automation.py

--- Office_Automation/Office_Automation.py
+++ Office_Automation/Office_Automation.py
@@ -29,13 +29,14 @@ from Extension_Modules import file_directory as fd
 from Extension_Modules import get_username as gu
 print("Application booting up successfully.")
 
-global ticker_list, outFile_1, outFile_2
+global rect_stock, ticker_list, path_1, path_2, outFile_1, outFile_2
+rect_stock = []
 ticker_list = []
 
 path_1 = fd.path_function("/rect_stock.dat")
 path_2 = fd.path_function("/email_info.dat")
-outFile_1 = open(path_1, "a+") if os.path.exists(path_1) else open(path_1, "w")
-outFile_2 = open(path_2, "a+") if os.path.exists(path_2) else open(path_2, "w")
+outFile_1 = open(path_1, "r") if os.path.exists(path_1) else open(path_1, "w")
+outFile_2 = open(path_2, "r") if os.path.exists(path_2) else open(path_2, "w")
 
 def check_ticker_exists(ticker):
     try:
@@ -79,7 +80,46 @@ def apply():
 
     stc_sym.set('')
 
+def stock_list():
+    if len(ticker_list) == 0:
+        messagebox.showerror("Error", "No stock symbol has been added.")
+        status_output_func("No stock symbol has been added.\n")
+    
+    else:
+        rect_stock_output_func("\n".join(ticker_list))
+
+def application():
+    status_output_func("Application is running...")
+    if len(ticker_list) == 0:
+        messagebox.showerror("Error", "No stock symbol has been added.")
+        status_output_func("No stock symbol has been added.\n")
+    
+    else:
+        messagebox.showinfo("Information", "Application is running...")
+        outFile_1 = open(path_1, "a+")
+        outFile_1.write("\n".join(ticker_list))
+        outFile_1.close()
+        status_output_func("Stock symbols have been saved.\n")
+
+    date_from_ = date_from.get()
+    date_to_ = date_to.get()
+    PTC_ = PTC.get()
+    VC_ = VC.get()
+    CC_ = CC.get()
+    MAC_ = MAC.get()
+    RSI_ = RSI.get()
+    BOL_ = BOL.get()
+    email_func_ = email_func.get()
+    SMTP_ = SMTP.get()
+    email_add_ = email_add.get()
+    password_ = password.get()     
+
 def main():
+    rect_stock = outFile_1.readlines()
+    for i in range(len(rect_stock)):
+        print(i)
+        rect_stock[i] = str(rect_stock[i]).rstrip('\n')
+
     win = ttk.Window(themename="cerculean")
     win.title("Office Automation - Automatic Stock Data Analysis")
     win.geometry("1280x720")
@@ -123,6 +163,9 @@ def main():
     rect_stock_output.config(state="disabled")
     tk.Button(win, text="Apply All", font=("Arial", 20)).place(x=10, y=420, width=620, height=40)
 
+    print(rect_stock)
+    rect_stock_output_func("\n".join(rect_stock))
+
     tk.Label(win, text="Date From", font=("Arial", 20)).place(x=640, y=85)
     tk.Entry(win, font=("Arial", 20), textvariable=date_from).place(x=640, y=130, width=310, height=40)
     tk.Label(win, text="Date To", font=("Arial", 20)).place(x=960, y=85)
@@ -149,9 +192,9 @@ def main():
     status_output.place(x=10, y=520, width=1260, height=100)
     status_output.config(state="disabled")
 
-    tk.Button(win, text="Stock List", font=("Arial", 20)).place(x=10, y=635, width=200, height=40)
+    tk.Button(win, text="Stock List", font=("Arial", 20), command=stock_list).place(x=10, y=635, width=200, height=40)
     tk.Button(win, text="Search - Yahoo Finance", font=("Arial", 20)).place(x=220, y=635, width=400, height=40)
-    tk.Button(win, text="Run Application", font=("Arial", 20)).place(x=630, y=635, width=640, height=40)
+    tk.Button(win, text="Run Application", font=("Arial", 20), command=application).place(x=630, y=635, width=640, height=40)
 
     win.mainloop()
 
